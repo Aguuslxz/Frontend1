66aa621561ff529e29b7c76b12c76b2cac648223
agustin <aagustinlopez0@gmail.com>
2023-02-17 20:17:23 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,69 @@
+let productList = [];
+let carrito = [];
+let total = 0;
+
+function add(productId, price) {
+   const product = productList.products.find((p) => p._id === productId);
+  
+  
+  console.log(productId, price);
+  carrito.push(productId);
+  total = total + price;
+  document.getElementById("checkout").innerHTML = `Pagar $${total}`
+  displayProducts();
+}
+
+async function pay() {
+  try{
+    const productList = await (await fetch("/api/pay",{
+    method:"post",
+    body: JSON.stringify(carrito),
+    headers: {
+      "Content-Type": "application/json" 
+    }
+  })).json();
+  }
+  catch{
+    window.alert("Sin stock");
+  }
+   
+  carrito = [];
+  total = 0;
+  await fetchProducts();
+  document.getElementById("checkout").innerHTML = `Pagar $${total}`;
+  
+   
+}
+
+//.....
+function displayProducts(productList) {
+  let productsHTML = '';
+  if(!productList.products.length > 0)return null 
+  productList.products.forEach(p => {
+      let buttonHTML = `<button class="button-add" onclick="add(${p._id}, ${p.price})">Agregar</button>)`;
+
+  
+      productsHTML += 
+      `<div class="product-container">
+          <h3>${p.name}</h3>
+          <img src="${p.image}" />
+          <h1>$${p.price}</h1>
+           ${buttonHTML}
+          </div>`;
+  });
+
+  document.getElementById("page-content").innerHTML = productsHTML;
+
+}
+
+async function fetchProducts(){
+   productList = await (
+    await fetch("https://backend-production-5b35.up.railway.app/products")
+   ).json();
+   displayProducts(productList);
+}
+
+window.onload = async()=> {
+  await fetchProducts();
+  
+}
